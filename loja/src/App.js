97e01c8216a61ac4ff490454c9c705a4858da62e
Otaciliojunior86 <otajunior1986@gmail.com>
2024-08-16 14:07:49 -0300

    atualize

--- loja/src/App.js
+++ loja/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Loja 7
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Loja 7</h1>
+
     </div>
   );
 }
